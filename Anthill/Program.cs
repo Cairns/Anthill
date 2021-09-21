46a8fd7b04f8064511cdec1b93bd395aa735e800
Andrew Cairns <cairns_agc@hotmail.com>
2021-09-21 22:07:49 +0100

    add factorial sum calculation

--- Anthill/Program.cs
+++ Anthill/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Anthill
 {
@@ -6,7 +11,58 @@ namespace Anthill
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number = GetNumber();
+
+            int factorial = Factorial(number);
+
+            Console.WriteLine($"The factorial sum of {number} is {factorial}");
+
+            Exit();
+        }
+
+        private static int GetNumber()
+        {
+            int number;
+            Console.WriteLine("Please enter a number");
+            string input = CaptureInput();
+
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine($"Invalid Input: {input}");
+                return 0;
+            }
+            {
+                return number;
+            }
+        }
+
+        private static string CaptureInput()
+        {
+            return Console.ReadLine();
+        }
+
+        private static int Factorial(int number)
+        {
+            if (number == 0)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+
+        private static void Pause(int seconds)
+        {
+            TimeSpan timeSpan = new TimeSpan(0, 0, seconds);
+            Thread.Sleep(timeSpan);
+        }
+
+        private static void Exit()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("Exiting the program");
+            Pause(2);
+            Environment.Exit(0);
         }
     }
 }
+
