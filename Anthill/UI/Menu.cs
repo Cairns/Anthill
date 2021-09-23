cdf0338a376c11bf5af7ae89743ab0d807941f90
Andrew Cairns <cairns_agc@hotmail.com>
2021-09-23 12:24:29 +0100

    Add static menu system

---
+++ Anthill/UI/Menu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anthill.UI
+{
+    public class Menu
+    {
+        public static void DisplayMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Factorial digit sum");
+            Console.WriteLine("2) Even fibonacci numbers");
+            Console.WriteLine("3) Multiples of 3 or 5");
+            Console.WriteLine("");
+            Console.WriteLine("0) Exit");
+            Console.WriteLine("");
+            Console.Write("\r\nSelect an option: ");
+        }
+    }
+}
