63b43e1bbc8f996a8c899ede0b74f52345743776
Andrew Cairns <cairns_agc@hotmail.com>
2021-09-25 20:47:25 +0100

    Add implementation of the three or five problem

--- Anthill/EulerProblems/MultiplesOfThreeOrFiveProblem.cs
+++ Anthill/EulerProblems/MultiplesOfThreeOrFiveProblem.cs
@@ -1,6 +1,7 @@
 ﻿using Anthill.EulerProblems.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace Anthill.EulerProblems
@@ -8,6 +9,9 @@ namespace Anthill.EulerProblems
     public class MultiplesOfThreeOrFiveProblem : IProblemDescriber, IProblemInitialiser, IProblemCalculator, IProblemDigitAggregator, IProblemResultFormatter
     {
         private int Number { get; set; }
+        private List<BigInteger> NaturalSequence { get; set; } = new List<BigInteger>();
+        private List<BigInteger> ThreeFiveSequence { get; set; } = new List<BigInteger>();
+        private BigInteger DigitSum { get; set; }
 
         public MultiplesOfThreeOrFiveProblem()
         {
@@ -25,22 +29,61 @@ namespace Anthill.EulerProblems
 
         public void Calculate()
         {
-            throw new NotImplementedException();
+            for(int i = 1; i < Number; i++)
+            {
+                NaturalSequence.Add(i);
+            }
         }
 
         public string Describe()
         {
-            throw new NotImplementedException();
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("");
+            builder.AppendLine("Multiples of Three or Five");
+            builder.AppendLine("");
+            builder.AppendLine("If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.The sum of these multiples is 23.");
+            builder.AppendLine("");
+            builder.AppendLine("Find the sum of all the multiples of 3 or 5 below 1000.");
+            builder.AppendLine("");
+            builder.AppendLine("Lets give it a try!");
+            return builder.ToString();
         }
 
         public string FormatOutputResults()
         {
-            throw new NotImplementedException();
+            var naturalSequence = string.Join(", ", NaturalSequence);
+            var problemSequence = string.Join(", ", ThreeFiveSequence);
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("");
+            builder.AppendLine($"You supplied the number {Number}");
+            builder.AppendLine("");
+            builder.AppendLine($"The natural sequence of numbers is {naturalSequence}");
+            builder.AppendLine("");
+            builder.AppendLine($"The sequence of multiples of 3 or 5 is {problemSequence}");
+            builder.AppendLine("");
+            builder.AppendLine($"The sum of all the multples of 3 or 5 below {Number} is {DigitSum} ");
+            return builder.ToString();
         }
 
         public void Sum()
         {
-            throw new NotImplementedException();
+            DigitSum = 0;
+
+            foreach (var number in NaturalSequence)
+            {
+                if(number % 3 == 0 ||
+                    number % 5 == 0)
+                {
+                    ThreeFiveSequence.Add(number);
+                    DigitSum += number;
+                }
+
+                if (number % 3 != 0 && number % 5 != 0)
+                {
+                    //Ignore
+                }
+            }
         }
     }
 }
