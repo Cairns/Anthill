db217a7e5e58c22f889c8b82b45c8439184c8aea
Andrew Cairns <cairns_agc@hotmail.com>
2021-09-25 19:34:58 +0100

    add extension isodd

---
+++ Anthill/Extensions/BigIntegerExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Anthill.Extensions
+{
+    public static class BigIntegerExtensions
+    {
+        public static bool IsOdd(this BigInteger number)
+        {
+            return !number.IsEven;
+        }
+    }
+}
